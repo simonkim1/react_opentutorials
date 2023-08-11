ed186f85cb604f2a0c898e6ba6fe495665dfbc5d
simonkim1 <sevencuter@gmail.com>
2023-08-11 10:21:48 +0900

    컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -4,10 +4,29 @@ function Header() {
   </header>
 }
 
+function Nav() {
+  return <nav>
+    <ol>
+      <li><a href="/read/1">html</a></li>
+      <li><a href="/read/2">css</a></li>
+      <li><a href="/read/3">js</a></li>
+    </ol>
+  </nav>
+}
+
+function Article() {
+  return <article>
+    <h2>Welcome</h2>
+    Hello, WEB
+  </article>
+}
+
 function App() {
   return (
     <div>
       <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
     </div>
   );
 }
